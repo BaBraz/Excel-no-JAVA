cc9ab26bd567c7fa668e5e61f05df0c92c335661
BaBraz <araujobraz.alexandre@gmail.com>
2022-11-21 23:23:29 -0300

    commit initial

---
+++ dataexcel/src/main/java/com/example/bruno/dataexcel/DataexcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.bruno.dataexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataexcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataexcelApplication.class, args);
+	}
+
+}
